93edc737497c7b2fcec439061398376bb0ae592a
Joey Sabani <axhi@users.noreply.github.com>
2014-08-27 16:54:23 -0500

    removed controller logic from model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,16 +3,5 @@ class Article < ActiveRecord::Base
 
   acts_as_taggable
   belongs_to :user
-
-  before_update :increase_vote_count
-  before_save :add_user_id
-
-  private
-
-  def increase_vote_count
-  end
-
-  def add_user_id
-  end
   
 end
