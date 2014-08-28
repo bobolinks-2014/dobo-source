a5f5fb32488d5c0383ba0b1bcd2fe2b8fd9ad197
Joey Sabani <joey.sabani@gmail.com>
2014-08-27 20:01:54 -0500

    added user, comment, and article rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
 
 describe User do
+  describe 'create a new user' do
 
+    it 'should require an email' do
+      
+    end
+
+    it 'password and password confirmation should match' do
+
+    end
+
+    it 'password must be more than eight characters long' do
+
+    end
+
+    it 'email must be unique' do
+
+    end
+
+  end
 end
