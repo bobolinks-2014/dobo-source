9012a1cc7b086d798ed6446454c7e6881158ab51
Jennie Chamberlin <chamberlin.jennie@gmail.com>
2014-08-30 21:30:05 -0500

    added phase tags to create method, added edit and destroy methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,9 +12,20 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     
-    if @article.save
+    # TODO: fix error message for phase tags to include all problems
+    # TODO: flash alert persists through redirect
+    if params["phase_tag"] == nil
+      errors = ["You must select a phase tag!"]
+      flash.now[:alert] = errors.join(', ')
+      render "new"
+    elsif @article.save
+      params["phase_tag"].each do |phase_tag|
+        @article.tag_list.add(phase_tag)
+      end
+      @article.save
       @article.update(poster: current_user)
-      redirect_to article_comments_path(@article)
+      redirect_to article_path(@article)
+      # redirect_to article_comments_path(@article)
     else
       flash[:alert] = @article.errors.full_messages.join(",")
       render "new"
@@ -26,11 +37,31 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-   @article = Article.find(params[:id])
+    @article = Article.find(params[:id])
+  end
+
+  def update 
+    @article = Article.find(params[:id])
+    if params["phase_tag"] == nil
+      errors = ["You must select a phase tag!"]
+      flash.now[:alert] = errors.join(', ')
+      render "new"
+    elsif @article.update(article_params)
+      params["phase_tag"].each do |phase_tag|
+        @article.tag_list.add(phase_tag)
+      end
+      @article.save
+      redirect_to article_path(@article)
+    else
+      flash.now[:alert] = @article.errors.full_messages.join(",")
+      render "edit"
+    end
   end
   
   def destroy
     @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to root_path
   end
 
 
