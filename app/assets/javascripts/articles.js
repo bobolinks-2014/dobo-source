596d86dcea55e349cce1fc3f6cc82ce7268305c8
Jennie Chamberlin <chamberlin.jennie@gmail.com>
2014-08-31 18:00:15 -0500

    added javascript to hide phase tags in tag list when editing an article

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,14 +1,19 @@
 $(document).ready(function() {
-  	var tagList = $("#article_tag_list").val().split(", ");
-  	var phaseTagList = ["phase0", "phase1", "phase2", "phase3", "alumni"];
-  	var newTags = [];
-  	
-		for (var i = 0; i < tagList.length; i++){
-			if (phaseTagList.indexOf(tagList[i]) == -1 ) {
-				newTags.push(tagList[i]);
+
+  	editURL = /articles\/\d*\/edit/;
+  		if (editURL.test(window.location.pathname)) {
+	  	console.log("i'm here!")
+	  	var tagList = $("#article_tag_list").val().split(", ");
+	  	var phaseTagList = ["phase0", "phase1", "phase2", "phase3", "alumni"];
+	  	var newTags = [];
+	  	
+			for (var i = 0; i < tagList.length; i++){
+				if (phaseTagList.indexOf(tagList[i]) == -1 ) {
+					newTags.push(tagList[i]);
+				};
 			};
+
+
+			$("#article_tag_list").val(newTags.join(", "));
 		};
-
-
-		$("#article_tag_list").val(newTags.join(", "));
-});
+	});
