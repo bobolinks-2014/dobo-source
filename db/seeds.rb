f8d60e89791028e536399887a048a047ed4f9bc7
Sebastian Radloff <sradloff23@gmail.com>
2014-08-31 15:43:52 -0500

    first round of creating project db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,31 @@
-User.create!(email: "tom@tom.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+require 'faker'
 
-User.create!(email: "john@john.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+User.create!(first_name: "Tom", last_name: "Thompson", cohort_animal: "Bobolinks", cohort_year: "2014", email: "tom@tom.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+User.create!(first_name: "John", last_name: "Johnson", cohort_animal: "Bobolinks", cohort_year: "2014", email: "john@john.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+User.create!(first_name: "Bob", last_name: "Bobson", cohort_animal: "Bobolinks", cohort_year: "2014", email: "bob@bob.com", password: "asdfasdf", password_confirmation: "asdfasdf")
 
-for i in 0..9
-	Article.create(title: "article title #{i}", short_description: "short asdf description", body: "astadf asdf asdfasd asdfasdf asdf asdf", user_id: User.find_by(email: "john@john.com").id)
-	
+50.times do
+	User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, cohort_animal: ["Bobolinks", "Mantises", "Herons", "Salamanders", "Coyotes"].sample , cohort_year: "2014", email: Faker::Internet.email, password: "asdfasdf", password_confirmation: "asdfasdf")
+end
+
+100.times do
+	Article.create(title: Faker::Company.catch_phrase, short_description: Faker::Lorem.sentence(2), body: Faker::Lorem.sentence(9), user_id: User.find(rand(1..50)).id)
+end
+
+100.times do
+	Article.create(title: Faker::Company.catch_phrase, short_description: Faker::Lorem.sentence(2), url: Faker::Internet.url, user_id: rand(1..50))
+end
+
+1000.times do
+	Comment.create(article_id: rand(1..200), user_id: rand(1..50), body: Faker::Lorem.sentence(6))
+end
+
+2000.times do
+	vote = Vote.new(tally_type: "Article", tally_id: rand(1..200), user_id: rand(1..50))
+	vote.save
+end
+
+4000.times do
+	vote = Vote.new(tally_type: "Comment", tally_id: rand(1..1000), user_id: rand(1..50))
+	vote.save
 end
