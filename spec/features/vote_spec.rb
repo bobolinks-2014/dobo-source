6e1bef56bcafdd4b4ee2e2cd71072369539b8f6f
Sebastian Radloff <sradloff23@gmail.com>
2014-08-31 01:10:51 -0500

    added comment voting tests, need html-css-comment-vote branch to merge to master, before tests will pass

--- spec/features/vote_spec.rb
+++ spec/features/vote_spec.rb
@@ -3,7 +3,7 @@ include RequestHelpers
 include WaitForAjax
 Capybara.javascript_driver = :webkit
 
-feature 'voting on article on main page'do
+feature 'voting on an article on main page'do
 	let(:user) { create_user }
 
 	before do
@@ -12,18 +12,46 @@ feature 'voting on article on main page'do
 		visit root_path
 	end
 
-	scenario 'user votes on article for the first time', js: true  do
+	scenario 'user votes on an article for the first time', js: true  do
 		click_on("niiiiiice")
 		wait_for_ajax
 		expect(page).to have_content("Thanks for voting!")
 		expect(page).to have_content(1)	
 	end
 
-	scenario 'user votes on article for the second time', js: true do
+	scenario 'user votes on an article for the second time', js: true do
 		click_on("niiiiiice")
 		visit root_path
 		click_on("niiiiiice")
+		wait_for_ajax
 		expect(page).to have_content("You previously voted on that")
 		expect(page).to have_content(1)	
 	end
+end
+
+feature 'voting on comments on article page' do
+	let(:user) { create_user }
+	let(:article) {Article.create!(title: "New Article", short_description: "this is cool", user_id: user.id, tag_list: "phase1")}
+
+	before do
+		login(user)
+		Comment.create(article: article, user_id: user.id, body: "this is a comment")
+		visit article_path(article)
+	end
+
+	scenario 'user votes on a comment for the first time', js: true do
+		click_on("niiiiiice")
+		wait_for_ajax
+		expect(page).to have_content("Thanks for voting!")
+		expect(page).to have_content(1)	
+	end
+
+	scenario 'user votes on a comment for the first time', js: true do
+		click_on("niiiiiice")
+		visit article_path
+		wait_for_ajax
+		expect(page).to have_content("Thanks for voting!")
+		expect(page).to have_content(1)	
+	end
+
 end
